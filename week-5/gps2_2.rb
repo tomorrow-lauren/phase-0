a20bd718ae7240591825d25fba419d5c5ac5ed25
Lauren Reid <lauren.g.reid@gmail.com>
2015-12-09 03:04:15 -0600

    update with more ideas

--- week-5/gps2_2.rb
+++ week-5/gps2_2.rb
@@ -1,4 +1,3 @@
-
 def grocery_list(items)
   the_list = Hash.new(0)
   items.split(" ").each {|x|
@@ -9,28 +8,35 @@ end
 
 puts grocery_list("eggs eggs")
 
-
-# set default quantity
-# print the list to the console: when
-# output: should be a hash
-# def add_to
-
-# Method to add an item to a list
-# input: item name and optional quantity
-# steps: it should take the items and their value and add them to the hash
+def add_to (new_item, quantity)
+# input: item name and optional quantity [new_item, quantity]
+# steps: it should take the items it should also check if it is in there and add if it is. and their value and add them to the hash
+# like the_list.each do |item| if item = new_item then the_list[item] += quantity.to_i
+# p "this was already here, so we added more!"
+#else the_list[new_item] = quantity.to_i
+#end end
 # output: a hash containing the input and everything else.
+end
 
-# Method to remove an item from the list
+def remove_item()
 # input:items item name and optional quantity
 # steps: find key, remove key and value return updated hash
+# like the last time but
+# the_list.each do |item| if item = new_item then the_list[item] = 0
+#end end
 # output: p updated hash
+end
 
-# Method to update the quantity of an item
+def update_number(item, quantity)
 # input: give item and new amount
 # steps: take item and change value to new amount
+# again, like:
+# the_list.each do |item| if item = new_item then the_list[item] = quantity.to_i
+#end end
 # output: p updated hash
 
-# Method to print a list and make it look pretty
-# input: takes the hash
+def print_list(hash)
+# input: takes the hash as input
 # steps: takes the key and prints it neatly next to it's value
 # output: something like p "key: {#value}"
+end
