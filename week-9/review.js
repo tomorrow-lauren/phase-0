2529878a8ab14297447c33e8f4026d50a0110375
Lauren Reid <lauren.g.reid@gmail.com>
2016-01-11 23:48:22 -0600

    Initial commit of 9.2

---
+++ week-9/review.js
@@ -0,0 +1,12 @@
+// psuedocode
+
+// a grocery list
+//
+
+// initial solution
+// refactor
+// reflection
+// What concepts did you solidify in working on this challenge?
+// What was the most difficult part of this challenge?
+// Did you solve the problem in a new way this time?
+// Was your pseudocode different from the Ruby version? What was the same and what was different?
